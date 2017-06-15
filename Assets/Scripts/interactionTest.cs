78fb825911087823c8b91a7661e0cdbd648877c9
Grayson Earle <graysonearle@gmail.com>
2017-06-15 17:27:35 -0400

    more commits

---
+++ Assets/Scripts/interactionTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionTest : MonoBehaviour {
+
+	public bool isLookedAt = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLookedAt) {
+			transform.Rotate (100 * Time.deltaTime, 0, 0);
+		}
+	}
+
+	public void LookChange() {
+		isLookedAt = !isLookedAt;
+	}
+
+}
--- Assets/Scripts/playVid.cs
+++ Assets/Scripts/playVid.cs
@@ -4,28 +4,30 @@ using UnityEngine;
 
 public class playVid : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown ("Jump")) {
-
-			Renderer r = GetComponent<Renderer>();
-			MovieTexture movie = (MovieTexture)r.material.mainTexture;
-
-			if (movie.isPlaying) {
-				movie.Pause();
-				GetComponent<AudioSource> ().Pause ();
-				GetComponent<Animator> ().Play ("screen-go-down");
-			}
-			else {
-				movie.Play();
-				GetComponent<AudioSource> ().Play ();
-				GetComponent<Animator> ().Play ("screen-come-up");
-			}
-		}
-	}
+//	MovieTexture movie;
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		if (Input.GetButtonDown ("Jump")) {
+//
+//			Renderer r = GetComponent<Renderer>();
+//			movie = (MovieTexture)r.material.mainTexture;
+//
+//			if (movie.isPlaying) {
+//				movie.Pause();
+//				GetComponent<AudioSource> ().Pause ();
+//				GetComponent<Animator> ().Play ("screen-go-down");
+//			}
+//			else {
+//				movie.Play();
+//				GetComponent<AudioSource> ().Play ();
+//				GetComponent<Animator> ().Play ("screen-come-up");
+//			}
+//		}
+//	}
 }
