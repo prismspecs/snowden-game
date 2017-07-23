465e11cedc9367fb48442f4cc72d4ab390893591
Grayson Earle <graysonearle@gmail.com>
2017-07-23 14:10:54 -0400

    major updates after meeting 2

---
+++ Assets/Scripts/ChipAway.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipAway : MonoBehaviour {
+
+	// Use this for initialization
+	public float time;
+	public int totalChips;
+	public 	ParticleSystem particle;
+	private float timeBetweenChips;
+	private float timePassed;
+	private int chipCount;
+	private bool isChipping;
+	private float initialHeight;
+	private Vector3 initialPos;
+	private float chipMagnitude;
+
+	void Start () {
+		isChipping = false;
+		timePassed = chipCount = 0;
+		initialPos = transform.position;
+		timeBetweenChips = time / totalChips;
+		initialHeight = transform.localScale.y;
+		chipMagnitude = initialHeight / totalChips;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isChipping) {
+			Chipping ();
+		}
+		if (timePassed > time) {
+			StopChipping ();
+		}
+	}
+
+	public void StartChipping(){
+		isChipping = true;
+	}
+
+	public void StopChipping(){
+		isChipping = false;
+		transform.localScale = new Vector3 (transform.localScale.x, initialHeight, transform.localScale.z);
+		transform.position = initialPos;
+		chipCount = 0;
+		timePassed = 0;
+
+	}
+
+	void Chipping(){
+		timePassed += Time.deltaTime;
+		if (chipCount < totalChips) {
+			if (timePassed >= timeBetweenChips) {
+				timePassed = 0;
+				chipCount++;
+				Chip ();
+			}
+		}
+	}
+
+	void Chip(){
+		transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y - chipMagnitude, 
+			transform.localScale.z);
+		transform.position = new Vector3 (transform.position.x, transform.position.y - (chipMagnitude / 2), transform.position.z);
+
+		particle.Play ();
+	}
+}
---
+++ Assets/Textures/NewUnlitShader.shader
@@ -0,0 +1,43 @@
+﻿ Shader "Custom/BInfiniteFog" {
+     Properties { 
+         _MainColour("Colour",Color)=(1,1,1,1)
+         _MainTex ("Base (RGB)", 2D) = "white" {}
+         _CutoffTex ("Alpha Guide", 2D) = "white" {}
+         _ScrollDir("Scroll Direction",Vector)=(1,0,0,0.1)
+         _FadeMul("Fading Multiplier",Range(0,3))=1
+     }
+     SubShader {
+         Tags { "RenderType"="Transparent" "RenderQueue"="Transparent"}
+         LOD 200
+         
+         CGPROGRAM
+         #pragma surface surf WrapLambert alpha
+         half4 LightingWrapLambert (SurfaceOutput s, half3 lightDir, half atten) {
+            //half NdotL = dot (s.Normal, lightDir);
+            half4 c;
+            c.rgb = s.Albedo * _LightColor0.rgb * (atten * 2);
+            c.a = s.Alpha;
+            return c;
+         }
+ 
+         sampler2D _MainTex;
+         sampler2D _CutoffTex;
+         fixed4 _ScrollDir;
+         fixed4 _MainColour;
+         half _FadeMul;
+         struct Input {
+             float2 uv_MainTex;
+             float2 uv_CutoffTex;
+             float4 screenPos;
+         };
+ 
+         void surf (Input IN, inout SurfaceOutput o) {
+             half4 c = tex2D (_MainTex, IN.uv_MainTex+_ScrollDir.xy*_ScrollDir.w*_Time.y)*_MainColour;
+             o.Emission = c.rgb;
+             o.Alpha=tex2D(_CutoffTex,IN.uv_CutoffTex).r*c.a;
+             o.Alpha*=clamp(IN.screenPos.z*0.4*_FadeMul,0,1);
+         }
+         ENDCG
+     } 
+     FallBack "Diffuse"
+ }
